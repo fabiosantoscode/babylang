6e360e295327651e0f78befb6a3736e55e81f8b2
Fábio Santos <fabiosantosart@gmail.com>
2019-01-01 22:35:48 +0000

    first commit

---
+++ test/index.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const assert = require('assert').strict
+const babylang = require('..')
+
+describe('babylang', () => {
+  it('parses assignment', () => {
+    assert.deepEqual(
+      babylang.parse('foo = x'),
+      'foo=x'
+    )
+  })
+  it('parses code', () => {
+    assert.deepEqual(
+      babylang.parse('foo x'),
+      'foo(x)'
+    )
+  })
+  it('parses multiple fargs', () => {
+    assert.deepEqual(
+      babylang.parse('foo x, y'),
+      'foo(x,y)'
+    )
+  })
+  it('allows spaces before comma', () => {
+    assert.deepEqual(
+      babylang.parse('foo x , y'),
+      'foo(x,y)'
+    )
+  })
+  it('can do if', () => {
+    assert.deepEqual(
+      babylang.parse('if foo {\n foo() \n}'),
+      'if(foo){foo()}'
+    )
+  })
+  it.skip('disallows nested functions', () => {
+    assert.throws(() => { babylang.parse('foo bar baz') }, /Do not write/)
+  })
+})
