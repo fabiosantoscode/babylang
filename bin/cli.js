f29542c5e984d79a1b278dea6b398a76a0d40ee6
Fábio Santos <fabiosantosart@gmail.com>
2019-01-02 16:27:00 +0000

    complete CLI

--- bin/cli.js
+++ bin/cli.js
@@ -1,27 +1,46 @@
+#!/usr/bin/env node
+
 'use strict'
 
-const fs = require('fs').promises
+const fs = require('fs')
 const cp = require('child_process')
-const assert = require('assert')
+const assert = require('assert').strict
 const babylang = require('..')
 const withTempFile = require('with-temp-file')
 
 const argv = require('minimist')(process.argv.slice(2))
 
-async function main() {
-  assert(argv.output, 'provide --output option')
+async function main () {
+  if (argv.help) {
+    console.log(`Usage:
+
+To run <file>:
+    $ babylang <file> --exec
+
+To compile <file> and put in <outfile>:
+    $ babylang <file> --output <outfile>
+`)
+
+    return
+  }
   assert.equal(argv._.length, 1, 'we only support a single file argument. do babylang [justonefile] ...')
-  console.log(await fs.readFile(argv._[0]) + '')
-  const script = babylang(await fs.readFile(argv._[0]) + '')
+  const script = babylang(fs.readFileSync(argv._[0]) + '')
 
   if (argv.exec) {
     await withTempFile(async (ws, filename) => {
       ws.end(script)
+      await new Promise(resolve => setTimeout(resolve, 500))
       const result = cp.spawnSync('node', [filename])
-      console.log({result})
+      if (result.stderr.toString().length) {
+        console.error(result.stderr.toString())
+      } else {
+        console.log(result.stdout.toString().replace(/\n$/m, ''))
+        console.log('(end of output)')
+      }
     })
   } else {
-    await fs.writeFile(argv.output, script)
+    assert(argv.output, 'provide --output option with a file')
+    fs.writeFileSync(argv.output, script)
   }
 }
 
--- test/index.js
+++ test/index.js
@@ -6,39 +6,39 @@ const babylang = require('..')
 describe('babylang', () => {
   it('parses assignment', () => {
     assert.deepEqual(
-      babylang.parse('foo = x'),
+      babylang('foo = x'),
       'foo=x'
     )
   })
   it('parses code', () => {
     assert.deepEqual(
-      babylang.parse('foo x'),
+      babylang('foo x'),
       'foo(x)'
     )
   })
   it('parses multiple fargs', () => {
     assert.deepEqual(
-      babylang.parse('foo x, y'),
+      babylang('foo x, y'),
       'foo(x,y)'
     )
   })
   it('allows spaces before comma', () => {
     assert.deepEqual(
-      babylang.parse('foo x , y'),
+      babylang('foo x , y'),
       'foo(x,y)'
     )
   })
   it('can do if', () => {
     assert.deepEqual(
-      babylang.parse('if foo {\n foo \n}'),
+      babylang('if foo {\n foo \n}'),
       'if(foo){foo()}'
     )
   })
   it.skip('disallows nested functions', () => {
-    assert.throws(() => { babylang.parse('foo bar baz') }, /Do not write/)
+    assert.throws(() => { babylang('foo bar baz') }, /Do not write/)
   })
   it('can do multiline programs', () => {
-    assert.deepEqual(babylang.parse(`
+    assert.deepEqual(babylang(`
         console.log 42, 123
 
         if 1 + 3 {
@@ -48,9 +48,12 @@ describe('babylang', () => {
       'console.log(42,123)if(1+3){console.log(1+3)}'
     )
   })
+  it('can do a simple program', () => {
+    assert.equal(babylang('console.log 123\n'), `console.log(123)`)
+  })
   it('breaks with unknown characters', () => {
     assert.throws(() => {
-      babylang.parse('foo =>')
+      babylang('foo =>')
     }, /^Error: Unknown character ">"$/)
   })
 })
